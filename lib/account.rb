6a15be1f6177403339762e9a9f77a5a995616b5a
Student <emilywright212@gmail.com>
2020-03-26 12:04:06 +0000

    Refactor - naming of deposit and withdrawal names separated

--- lib/account.rb
+++ lib/account.rb
@@ -7,39 +7,38 @@ class Account
     @balance = 0
   end
 
-  def make_deposit(input_amount, date)
-    @balance += input_amount
+  def make_deposit(deposit_amount, date)
+    @balance += deposit_amount
 
-    two_dp_amount = convert_two_dp(input_amount)
+    two_dp_deposit = convert_two_dp(deposit_amount)
     two_dp_balance = convert_two_dp(@balance)
 
-    save_deposit(two_dp_amount, date, two_dp_balance)
+    save_deposit(two_dp_deposit, date, two_dp_balance)
 
   end
 
-  def make_withdrawal(input_amount, date)
-    @balance -= input_amount
+  def make_withdrawal(withdrawal_amount, date)
+    @balance -= withdrawal_amount
 
-    two_dp_amount = convert_two_dp(input_amount)
+    two_dp_withdrawal = convert_two_dp(withdrawal_amount)
     two_dp_balance = convert_two_dp(@balance)
 
-    save_withdrawal(two_dp_amount, date, two_dp_balance)
+    save_withdrawal(two_dp_withdrawal, date, two_dp_balance)
 
   end
 
   private
 
-  def save_deposit(two_dp_amount, date, two_dp_balance)
-    @all_transactions << "#{date} || #{two_dp_amount} || || #{two_dp_balance}"
+  def save_deposit(two_dp_deposit, date, two_dp_balance)
+    @all_transactions << "#{date} || #{two_dp_deposit} || || #{two_dp_balance}"
   end
 
-  def save_withdrawal(two_dp_amount, date, two_dp_balance)
-    @all_transactions << "#{date} || || #{two_dp_amount} || #{two_dp_balance}"
+  def save_withdrawal(two_dp_withdrawal, date, two_dp_balance)
+    @all_transactions << "#{date} || || #{two_dp_withdrawal} || #{two_dp_balance}"
   end
 
   def convert_two_dp(float)
     two_dp_string = '%.2f' % float
   end
-  # TODO - rename input amounts, split this 2dp thing, 
-  # split save formatting, then write unit tests for these
+
 end
