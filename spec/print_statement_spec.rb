4021ffcee3b96e79df7dec972015047774f93c0f
Student <emilywright212@gmail.com>
2020-03-26 14:56:11 +0000

    Use of doubles for print statement spec

--- spec/print_statement_spec.rb
+++ spec/print_statement_spec.rb
@@ -1,11 +1,12 @@
 require 'bank_statement'
-describe 'gets output for bank statement' do
-  it 'print one line bank statment, no transactions' do
-    #TODO - doubles here as not feature test
-    account = Account.new
-    bank_statement = BankStatement.new(account.all_transactions)
+
+describe 'user can print a statement' do
+  let(:account) { double :account }
+  it 'no transactions have been made' do
+    allow(account).to receive(:all_transactions).and_return([])
+    bankstatement = BankStatement.new(account.all_transactions)
     expect do
-      bank_statement.print_statement
+      bankstatement.print_statement
     end.to output("date || credit || debit || balance\n").to_stdout
   end
 end
