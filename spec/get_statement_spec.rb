7595f3266c81082ff2cb27f3797e060ed1e1f5a6
Student <emilywright212@gmail.com>
2020-03-26 10:32:59 +0000

    Refactored tests for clarity

--- spec/get_statement_spec.rb
+++ spec/get_statement_spec.rb
@@ -2,6 +2,7 @@ require 'bank_statement'
 
 describe 'gets output for bank statement' do
   it 'print one line bank statment, no transactions' do
+    #TODO - doubles here as not feature test
     account = Account.new
     bank_statement = BankStatement.new(account.all_transactions)
     expect(bank_statement.get_statement).to eq("date || credit || debit || balance")
