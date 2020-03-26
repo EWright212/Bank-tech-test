44c7b489dc76229d194e6fa2719ef48217599428
Student <emilywright212@gmail.com>
2020-03-26 11:49:09 +0000

    Balance method in place with unit test

--- spec/make_deposit_spec.rb
+++ spec/make_deposit_spec.rb
@@ -10,4 +10,10 @@ describe 'user can make a deposit' do
     expect(account.make_deposit(200.00, "02/01/2020")).to eq(["02/01/2020 || 200.00 || || 200.00"])
     expect(account.all_transactions).to eq(["02/01/2020 || 200.00 || || 200.00"])
   end
+  it 'user makes one deposit and balance changed' do
+    account = Account.new
+    expect(account.balance).to eq(0)
+    expect(account.make_deposit(200.00, "02/01/2020")).to eq(["02/01/2020 || 200.00 || || 200.00"])
+    expect(account.balance).to eq(200.0)
+  end
 end
