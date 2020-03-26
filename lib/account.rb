d63d4f67032f4169fa2be07bb26b6f2543206abb
Student <emilywright212@gmail.com>
2020-03-26 10:37:52 +0000

    Can handle withdrawals

--- lib/account.rb
+++ lib/account.rb
@@ -13,6 +13,12 @@ class Account
 
   end
 
+  def make_withdrawal(input_amount, date)
+    two_dp_amount = ('%.2f' % input_amount)
+
+    @all_transactions = "#{date} || || #{two_dp_amount} || -#{two_dp_amount}"
+
+  end
   # def save_transaction
   # end
 
---
+++ spec/features/withdrawals_spec.rb
@@ -0,0 +1,13 @@
+require 'bank_statement'
+require 'account'
+
+describe 'user can make a deposit' do
+  it 'user makes one deposit then prints statment' do
+    account = Account.new
+    account.make_withdrawal(100.00, "01/01/2020")
+    bankstatement = BankStatement.new(account.all_transactions)
+    expect do
+      bankstatement.print_statement
+    end.to output("date || credit || debit || balance\n01/01/2020 || || 100.00 || -100.00\n").to_stdout
+  end
+end
