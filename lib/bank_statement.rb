d46dc3e36524c1b596424b486f4a4b7908c03eef
Student <emilywright212@gmail.com>
2020-03-26 12:07:56 +0000

    Refactor bank statment

--- lib/bank_statement.rb
+++ lib/bank_statement.rb
@@ -1,30 +1,14 @@
 class BankStatement
 
   def initialize(all_transactions)
-    @list_of_transactions = all_transactions
+    @array_of_transactions = all_transactions
   end
 
   def print_statement
     puts "date || credit || debit || balance"
-    if @list_of_transactions == []
-    else
-      @list_of_transactions.reverse.each do |transaction|
-        puts transaction.inspect.gsub!(/\A"|"\Z/, '')
-      end
+    @array_of_transactions.reverse.each do |transaction|
+      puts transaction.inspect.gsub!(/\A"|"\Z/, '')
     end
   end
 
-  # def get_statement
-    
-  #   if @list_of_transactions == []
-  #     output = ""
-  #   else
-  #     @list_of_transactions.each do |transaction|
-  #       output = transaction.inspect
-  #     end
-  #     output = "\n" + output.gsub!(/\A"|"\Z/, '')
-  #   end
-
-  #   "date || credit || debit || balance#{output}"
-  # end
 end
