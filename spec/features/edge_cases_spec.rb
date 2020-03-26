8c1814fb100ebaf5a0b49be0bcb77976a03ed630
Student <emilywright212@gmail.com>
2020-03-26 16:46:22 +0000

    Edge case testing for withdrawal of 0.00

--- spec/features/edge_cases_spec.rb
+++ spec/features/edge_cases_spec.rb
@@ -10,4 +10,12 @@ describe 'edge cases' do
       bankstatement.print_statement
     end.to output("date || credit || debit || balance\n01/01/2020 || 0.00 || || 0.00\n").to_stdout
   end
+  it 'deposit of 0.00 made' do
+    account = Account.new
+    account.make_withdrawal(0.00, "01/01/2020")
+    bankstatement = BankStatement.new(account.all_transactions)
+    expect do
+      bankstatement.print_statement
+    end.to output("date || credit || debit || balance\n01/01/2020 || || 0.00 || 0.00\n").to_stdout
+  end
 end
