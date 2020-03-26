f30465d16056cfb158861654abf3a3dcc4bc01ca
Student <emilywright212@gmail.com>
2020-03-26 16:40:43 +0000

    Use of double for printing multiple line statement

--- spec/print_statement_spec.rb
+++ spec/print_statement_spec.rb
@@ -10,3 +10,14 @@ describe 'user can print a statement' do
     end.to output("date || credit || debit || balance\n").to_stdout
   end
 end
+
+describe 'user can print a statement' do
+  let(:account) { double :account }
+  it 'no transactions have been made' do
+    allow(account).to receive(:all_transactions).and_return(["10/01/2012 || 1000.00 || || 1000.00",  "13/01/2012 || 2000.00 || || 3000.00", "14/01/2012 || || 500.00 || 2500.00"])
+    bankstatement = BankStatement.new(account.all_transactions)
+    expect do
+      bankstatement.print_statement
+    end.to output("date || credit || debit || balance\n14/01/2012 || || 500.00 || 2500.00\n13/01/2012 || 2000.00 || || 3000.00\n10/01/2012 || 1000.00 || || 1000.00\n").to_stdout
+  end
+end
